b07a5fb0785c087e3e04430a0c5d0ad376eaed32
Danny Peck <dannytpeck@gmail.com>
2018-02-13 12:03:10 -0800

    Remove extra selector

--- src/components/kiln.jsx
+++ src/components/kiln.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Selectors from './selectors';
+import SelectChallengeType from './select_challenge_type';
 import UploadModal from './upload_modal';
 import CustomSelfReport from './custom_self_report';
 import CustomCie from './custom_cie';
@@ -35,7 +35,7 @@ class Kiln extends React.Component {
 			<div>
 				<h2>The Kiln</h2>
 				<p>What are we creating?</p>
-				<Selectors onChange={this.handleSelectType.bind(this)} />
+        <SelectChallengeType onChange={this.handleSelectType.bind(this)} />
 				{content}
 				<input id="titleInput" type="text" placeholder="Library Post Title"/>
 				<button className="btn btn-primary" id="upload" onClick={this.handleUploadClick}>Create Library Post</button>
