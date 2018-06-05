90e35e44690661b351d4c99a66c1571b984a66f6
Josh Braun <josh.braun@adurolife.com>
2018-06-05 15:23:22 -0700

    Refactor json_details into component from functional component

--- src/components/self_report_json_details.jsx
+++ src/components/self_report_json_details.jsx
@@ -1,62 +1,68 @@
 import React from 'react';
 
-const SelfReportJsonDetails = () => {
-  return (
-    <div id="jsonDetails">
-      <h4>JSON Object:</h4>
+class SelfReportJsonDetails extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-      <form id="form jsonEdit">
+  render() {
+    return (
+      <div id="jsonDetails">
+        <h4>JSON Object:</h4>
 
-        <div className="form-group">
-          <label htmlFor="jsonDimensions">Dimensions</label>
-          <input type="text" className="form-control" id="jsonDimensions" placeholder='"Resilience", "Self-Care"' />
-        </div>
+        <form id="form jsonEdit">
 
-        <div className="form-group">
-          <label htmlFor="jsonImageUrl">Image URL</label>
-          <input type="text" className="form-control" id="jsonImageUrl" placeholder="#" />
-        </div>
+          <div className="form-group">
+            <label htmlFor="jsonDimensions">Dimensions</label>
+            <input type="text" className="form-control" id="jsonDimensions" placeholder='"Resilience", "Self-Care"' />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="jsonTeam">Team</label>
-          <select id="jsonTeam" className="form-control custom-select">
-            <option defaultValue>Individual</option>
-            <option>Team</option>
-          </select>
-        </div>
+          <div className="form-group">
+            <label htmlFor="jsonImageUrl">Image URL</label>
+            <input type="text" className="form-control" id="jsonImageUrl" placeholder="#" />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="jsonTracking">Tracking</label>
-          <select id="jsonTracking" className="form-control custom-select">
-            <option defaultValue>One Time</option>
-            <option>One Time Days</option>
-            <option>One Time Units</option>
-            <option>Weekly Days</option>
-            <option>Weekly Units</option>
-          </select>
-        </div>
+          <div className="form-group">
+            <label htmlFor="jsonTeam">Team</label>
+            <select id="jsonTeam" className="form-control custom-select">
+              <option defaultValue>Individual</option>
+              <option>Team</option>
+            </select>
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="jsonRequired">Required (Times to Track)</label>
-          <input type="text" className="form-control" id="jsonRequired" placeholder="0" />
-        </div>
+          <div className="form-group">
+            <label htmlFor="jsonTracking">Tracking</label>
+            <select id="jsonTracking" className="form-control custom-select">
+              <option defaultValue>One Time</option>
+              <option>One Time Days</option>
+              <option>One Time Units</option>
+              <option>Weekly Days</option>
+              <option>Weekly Units</option>
+            </select>
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="jsonDevice">Device</label>
-          <select id="jsonDevice" className="form-control custom-select">
-            <option defaultValue>no</option>
-            <option>yes</option>
-          </select>
-        </div>
+          <div className="form-group">
+            <label htmlFor="jsonRequired">Required (Times to Track)</label>
+            <input type="text" className="form-control" id="jsonRequired" placeholder="0" />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="jsonText">Tracking Text</label>
-          <input type="text" className="form-control" id="jsonText" placeholder="daily activity OR steps | exercise OR miles | exercise" />
-        </div>
+          <div className="form-group">
+            <label htmlFor="jsonDevice">Device</label>
+            <select id="jsonDevice" className="form-control custom-select">
+              <option defaultValue>no</option>
+              <option>yes</option>
+            </select>
+          </div>
 
-      </form>
-    </div>
-  );
-};
+          <div className="form-group">
+            <label htmlFor="jsonText">Tracking Text</label>
+            <input type="text" className="form-control" id="jsonText" placeholder="daily activity OR steps | exercise OR miles | exercise" />
+          </div>
+
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SelfReportJsonDetails;
