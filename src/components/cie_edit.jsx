e42b88a64cd09fdb95a26294c8137f5d02fe27f9
Josh Braun <josh.braun@adurolife.com>
2018-06-06 17:13:22 -0700

    Remove category select for CIEs

--- src/components/cie_edit.jsx
+++ src/components/cie_edit.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import CategorySelect from './category_select';
 import EditBox from './edit_box';
 import TrumbowygBox from './trumbowyg_box';
-import GenerateCode from './generate_code';
 
 const CieEdit = ({ onKeyUp }) => {
   return (
     <div id="edit" onKeyUp={onKeyUp}>
-      <CategorySelect />
       <EditBox name="title" title="Title" characters="100" />
       <TrumbowygBox name="description" title="Description (HTML)" characters="2000" />
       {/* <GenerateCode type="cie" /> */}
