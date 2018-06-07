6575bab8a660db0609957a34d8af71600bca3f35
Josh Braun <josh.braun@adurolife.com>
2018-06-06 17:21:37 -0700

    Remove Generate Code button and no-longer-needed related code

--- src/components/cie_edit.jsx
+++ src/components/cie_edit.jsx
@@ -7,7 +7,6 @@ const CieEdit = ({ onKeyUp }) => {
     <div id="edit" onKeyUp={onKeyUp}>
       <EditBox name="title" title="Title" characters="100" />
       <TrumbowygBox name="description" title="Description (HTML)" characters="2000" />
-      {/* <GenerateCode type="cie" /> */}
     </div>
   );
 };
