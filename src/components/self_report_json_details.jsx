f84609fa52f559cb97879d5925ec9b36fa78fd3e
Josh Braun <josh.braun@adurolife.com>
2018-08-07 16:14:07 -0700

    Update Self Report JSON Details to match the Airtable format

--- src/components/self_report_json_details.jsx
+++ src/components/self_report_json_details.jsx
@@ -13,23 +13,66 @@ class SelfReportJsonDetails extends React.Component {
         <form id="form jsonEdit">
 
           <div className="form-group">
-            <label htmlFor="jsonDimensions">Dimensions</label>
-            <input type="text" className="form-control" id="jsonDimensions" placeholder='Resilience, Self-Care' />
+            <label htmlFor="jsonDimensions">Limeade Dimensions</label>
+            <input type="text" className="form-control" id="jsonDimensions" placeholder="Resilience, Self-Care" />
           </div>
 
           <div className="form-group">
-            <label htmlFor="jsonImageUrl">Image URL</label>
-            <input type="text" className="form-control" id="jsonImageUrl" placeholder="#" />
+            <label htmlFor="jsonImageUrl">Limeade Image URL</label>
+            <input type="text" className="form-control" id="jsonImageUrl" placeholder="/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-21-21-14-42/example.jpg" />
           </div>
 
           <div className="form-group">
-            <label htmlFor="jsonTeam">Team</label>
-            <select id="jsonTeam" className="form-control custom-select">
-              <option defaultValue>Individual</option>
-              <option>Team</option>
+            <label htmlFor="jsonTeamActivity">Team Activity?</label>
+            <select id="jsonTeamActivity" className="form-control custom-select">
+              <option defaultValue>no</option>
+              <option>yes</option>
             </select>
           </div>
 
+          <div className="form-group">
+            <label htmlFor="jsonRewardOccurrence">Reward Occurrence</label>
+            <select id="jsonRewardOccurrence" className="form-control custom-select">
+              <option defaultValue>Once</option>
+              <option>Weekly</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="jsonActivityTrackingType">Activity Tracking Type</label>
+            <select id="jsonActivityTrackingType" className="form-control custom-select">
+              <option defaultValue>Event</option>
+              <option>Days</option>
+              <option>Units</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="jsonActivityGoal">Activity Goal</label>
+            <input type="text" className="form-control" id="jsonActivityGoal" placeholder="1" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="jsonActivityGoalText">Activity Goal Text</label>
+            <input type="text" className="form-control" id="jsonActivityGoalText" placeholder="throw tables" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="jsonDeviceEnabled">Device Enabled?</label>
+            <select id="jsonDeviceEnabled" className="form-control custom-select">
+              <option defaultValue>no</option>
+              <option>yes</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="jsonDeviceUnits">Device Units</label>
+            <input type="text" className="form-control" id="jsonDeviceUnits" placeholder="steps" />
+          </div>
+
+
+          {/* old fields */}
+          {/*
           <div className="form-group">
             <label htmlFor="jsonTracking">Tracking</label>
             <select id="jsonTracking" className="form-control custom-select">
@@ -58,6 +101,7 @@ class SelfReportJsonDetails extends React.Component {
             <label htmlFor="jsonText">Tracking Text</label>
             <input type="text" className="form-control" id="jsonText" placeholder="daily activity OR steps | exercise OR miles | exercise" />
           </div>
+          */}
 
         </form>
       </div>
